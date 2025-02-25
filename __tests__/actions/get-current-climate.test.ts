b7e279ddf293bf56baee621f127dda0cb1b75e0d
Raul Almanza <ra.almanzaserrano@gmail.com>
2025-02-24 21:31:19 -0600

    Add tests for main page and get-current-climate action

---
+++ __tests__/actions/get-current-climate.test.ts
@@ -0,0 +1,40 @@
+import { getCurrentClimate } from '@/actions/get-current-climate';
+
+describe('Get current climate action', () => {
+  it('returns valid data when valid params are passed to it', async () => {
+    const response = await getCurrentClimate(20.571358, -101.192444);
+
+    expect(response).toMatchObject({
+      coord: expect.objectContaining({
+        lon: expect.any(Number),
+        lat: expect.any(Number),
+      }),
+      weather: expect.arrayContaining([
+        expect.objectContaining({
+          id: expect.any(Number),
+          main: expect.any(String),
+          description: expect.any(String),
+          icon: expect.any(String),
+        }),
+      ]),
+      main: expect.objectContaining({
+        temp: expect.any(Number),
+        feels_like: expect.any(Number),
+        temp_min: expect.any(Number),
+        temp_max: expect.any(Number),
+        pressure: expect.any(Number),
+        humidity: expect.any(Number),
+        sea_level: expect.any(Number),
+        grnd_level: expect.any(Number),
+      }),
+    });
+  });
+
+  it('throws and error if something is wrong with the API call', async () => {
+    global.fetch = jest.fn(() => Promise.reject(new Error('Server is down'))) as jest.Mock;
+
+    const response = getCurrentClimate(20.571358, -101.192444);
+
+    await expect(response).rejects.toThrow('Failed to fetch current weather information');
+  });
+});
--- __tests__/app/page.test.tsx
+++ __tests__/app/page.test.tsx
@@ -1,4 +1,3 @@
-import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 
 import Page from '@/app/page';
@@ -18,5 +17,31 @@ describe('Main Page', () => {
 
     expect(heading).toBeInTheDocument();
     expect(heading).toHaveTextContent('Weather Forecast');
+  });
+
+  it('renders search component', async () => {
+    const ResolvedPage = await resolvedComponent(Page);
+    render(<ResolvedPage />);
+
+    const heading = screen.getByText('Search for any city in the world and click on it');
+    const searchBar = screen.getByPlaceholderText('Enter location');
+
+    expect(heading).toBeInTheDocument();
+    expect(searchBar).toBeInTheDocument();
+  });
+
+  it('renders favorite cities component', async () => {
+    const ResolvedPage = await resolvedComponent(Page);
+    render(<ResolvedPage />);
+
+    const heading = screen.getByText('Favorite Cities');
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('matches snapshot', async () => {
+    const ResolvedPage = await resolvedComponent(Page);
+    const { asFragment } = render(<ResolvedPage />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
