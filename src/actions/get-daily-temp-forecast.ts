c3452f2d2278d635e6f14612f03421e8bf78bc81
Raul Almanza <ra.almanzaserrano@gmail.com>
2025-03-01 07:07:10 -0600

    Add internationalization error messages in server actions

--- src/actions/get-daily-temp-forecast.ts
+++ src/actions/get-daily-temp-forecast.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { getTranslations } from 'next-intl/server';
+
 import { Units } from '@/lib/definitions/requests';
 import { convertToDailyTempForecast } from '@/lib/utils';
 
@@ -10,10 +12,11 @@ export const getDailyTempForecast = async (
 ) => {
   const apiUrl = process.env.WEATHERAPP_API_URL;
   const apiKey = process.env.WEATHERAPP_API_KEY;
+  const t = await getTranslations('ActionErrors');
 
   try {
     if (!apiUrl || !apiKey) {
-      throw new Error('API URL or API key is missing');
+      throw new Error(t('missingApiKey'));
     }
     const forecastRes = await fetch(
       `${apiUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`
@@ -25,6 +28,6 @@ export const getDailyTempForecast = async (
     return dailyTempForecast;
   } catch (error) {
     console.error(error);
-    throw new Error('Failed to fetch daily temperature information');
+    throw new Error(t('getDailyTempForecastError'));
   }
 };
--- src/actions/search-city-location.ts
+++ src/actions/search-city-location.ts
@@ -1,14 +1,17 @@
 'use server';
 
+import { getTranslations } from 'next-intl/server';
+
 import { City } from '@/lib/definitions/requests';
 
 export const searchCityLocation = async (query: string) => {
   const apiUrl = process.env.WEATHERAPP_API_URL;
   const apiKey = process.env.WEATHERAPP_API_KEY;
+  const t = await getTranslations('ActionErrors');
 
   try {
     if (!apiUrl || !apiKey) {
-      throw new Error('API URL or API key is missing');
+      throw new Error(t('missingApiKey'));
     }
 
     const response = await fetch(`${apiUrl}/geo/1.0/direct?q=${query}&limit=10&appid=${apiKey}`);
@@ -17,6 +20,6 @@ export const searchCityLocation = async (query: string) => {
     return data;
   } catch (error) {
     console.error(error);
-    throw new Error('Failed to search for city name');
+    throw new Error(t('searchCityLocationError'));
   }
 };
